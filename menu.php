03bac86c94057a227455a6cca2c76e372a80afb8
Marco Ortega <marco@tygonsoft.com>
2021-11-04 10:50:14 -0600

    Crea fichero unicamente para el menú

---
+++ menu.php
@@ -0,0 +1,37 @@
+<header>
+<div class="row bg-dark">
+    <div class="col-3 col-md-9 col-lg-10"></div>
+    <div class="col-lg-2 d-flex justify-content-center justify-content-lg-end justify-content-md-end">
+        <ul class="mt-2 mb-2 sin-estilos">
+            <li class="ms-1">
+                <a class="text-white" href="registro.html">Registro</a>
+            </li>
+            <li class="ms-3">
+                <a class="text-white" href="iniciar-sesion.html"> Iniciar Sesión</a>
+            </li>                                                  
+        </ul>
+    </div>
+</div>
+    
+</header>
+<nav class="row navbar navbar-expand-lg navbar-light bg-primary mb-1">
+    <div class="container-fluid">
+        <a class="navbar-brand col-lg-7" href="index.html"><img class="logo" src="img/logo.jpg" alt="Logo de prueba"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse col-lg-3 justify-content-lg-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active text-center" aria-current="page" href="productos.html">Productos y Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-center" aria-current="page" href="contacto.html">Contáctanos</a>
+                </li>
+                <li class="nav-item">
+                    <a aria-current="page" class="fas fa-shopping-cart nav-link active text-center" href="carrito.php"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
