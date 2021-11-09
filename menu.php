7ff3c0e0d0848ab5e9cb6481ce021889ad0e2391
Marco Ortega <marco@tygonsoft.com>
2021-11-09 08:01:26 -0600

    Actualiza direcciones de los ficheros

--- menu.php
+++ menu.php
@@ -4,10 +4,10 @@
     <div class="col-lg-2 d-flex justify-content-center justify-content-lg-end justify-content-md-end">
         <ul class="mt-2 mb-2 sin-estilos">
             <li class="ms-1">
-                <a class="text-white" href="registro.html">Registro</a>
+                <a class="text-white" href="registro.php">Registrarme</a>
             </li>
             <li class="ms-3">
-                <a class="text-white" href="iniciar-sesion.html"> Iniciar Sesión</a>
+                <a class="text-white" href="iniciar-sesion.php"> Iniciar Sesión</a>
             </li>                                                  
         </ul>
     </div>
@@ -16,17 +16,17 @@
 </header>
 <nav class="row navbar navbar-expand-lg navbar-light bg-primary mb-1">
     <div class="container-fluid">
-        <a class="navbar-brand col-lg-7" href="index.html"><img class="logo" src="img/logo.jpg" alt="Logo de prueba"></a>
+        <a class="navbar-brand col-lg-7" href="index.php"><img class="logo" src="img/logo.jpg" alt="Logo de prueba"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse col-lg-3 justify-content-lg-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active text-center" aria-current="page" href="productos.html">Productos y Servicios</a>
+                    <a class="nav-link active text-center" aria-current="page" href="productos.php">Productos y Servicios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active text-center" aria-current="page" href="contacto.html">Contáctanos</a>
+                    <a class="nav-link active text-center" aria-current="page" href="contacto.php">Contáctanos</a>
                 </li>
                 <li class="nav-item">
                     <?php
