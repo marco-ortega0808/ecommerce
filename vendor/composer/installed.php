ca91b6dc6ad131e9bf2d82506d3cbf16ba98a94b
Marco Ortega <marco@tygonsoft.com>
2021-11-09 06:39:14 -0600

    Descarga libreria de Stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a63c58778a3f9ac73f1726b7bcb0c74272c2f6de',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a63c58778a3f9ac73f1726b7bcb0c74272c2f6de',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.100.0',
+            'version' => '7.100.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '3dfc3dcd5d967a14d2852f34e544188af5f9b799',
+            'dev_requirement' => false,
+        ),
+    ),
+);
